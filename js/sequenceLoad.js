2cd4f48e347544fc32a0597f8724d6815e42e56a
Jay Mo <jay@jaymo.io>
2019-10-21 01:52:55 -0400

    Update underlay animation

--- js/sequenceLoad.js
+++ js/sequenceLoad.js
@@ -3,7 +3,13 @@ const onLoad = function() {
 
 	let timelineLoad = new TimelineMax({ paused: true })
 		.set(document.querySelector('#animation-cover'), { display: 'none' })
-		.set(document.querySelector('.underlay'), { opacity: 0 })
+		// .set(document.querySelector('.underlay'), { opacity: 0 })
+		.add(function() {
+			for (let i = 0; i < 10; i++) {
+				TweenMax.set(document.getElementsByClassName('underlay__row')[i], { opacity: 0 })
+			}
+		})
+		// .set(document.getElementsByClassName('underlay__row')[0], { opacity: 0 })
 		.set(document.querySelector('.content__title__english__content'), { yPercent: 100 })
 		.set(document.querySelector('.content__title__korean__content'), { yPercent: 200 })
 		.set(document.getElementsByClassName('content__group__line__content')[0], { yPercent: 100 })
@@ -25,11 +31,25 @@ const onLoad = function() {
 		.to(document.getElementsByClassName('content__group__line__content')[5], 0.8, { ease: 'Mo', yPercent: 0 }, '-=0.775')
 		.to(document.getElementsByClassName('content__group__line__content')[6], 0.8, { ease: 'Mo', yPercent: 0 }, '-=0.775')
 		.to(document.getElementsByClassName('content__group__line__content')[7], 0.8, { ease: 'Mo', yPercent: 0 }, '-=0.775')
-		.to(document.getElementsByClassName('content__group__line__content')[8], 0.8, { ease: 'Mo', yPercent: 0 }, '-=0.775')
-		.to(document.querySelector('.underlay'), 0.8, { ease: 'Mo', opacity: 1 }, '-=0.775');
+		.to(document.getElementsByClassName('content__group__line__content')[8], 0.8, { ease: 'Mo', yPercent: 0 }, '-=0.775');
+		// .to(document.querySelector('.underlay'), 0.8, { ease: 'Mo', opacity: 1 }, '-=0.775');
 
+	let timelineUnderlay = new TimelineMax({ paused: true })
+		.to({}, 0.45, {})
+		.to(document.getElementsByClassName('underlay__row')[4], 0.8, { ease: 'Mo', opacity: 0.52 })
+		.to(document.getElementsByClassName('underlay__row--flipped')[0], 0.8, { ease: 'Mo', opacity: 0.52 }, '-=0.8')
+		.to(document.getElementsByClassName('underlay__row')[3], 0.8, { ease: 'Mo', opacity: 0.64 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row--flipped')[1], 0.8, { ease: 'Mo', opacity: 0.64 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row')[2], 0.8, { ease: 'Mo', opacity: 0.76 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row--flipped')[2], 0.8, { ease: 'Mo', opacity: 0.76 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row')[1], 0.8, { ease: 'Mo', opacity: 0.88 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row--flipped')[3], 0.8, { ease: 'Mo', opacity: 0.88 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row')[0], 0.8, { ease: 'Mo', opacity: 1 }, '-=0.75')
+		.to(document.getElementsByClassName('underlay__row--flipped')[4], 0.8, { ease: 'Mo', opacity: 1 }, '-=0.75');
+	
 
 	timelineLoad.play();
+	timelineUnderlay.play();
 };
 
 if (
